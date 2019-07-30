cd597aa311bf79e1667d4a59ccd76fe0004cabdd
黄思佳 <huangsijia@easybaocn.com>
2019-07-30 15:02:33 +0800

    commit

--- src/main/java/com/study/bigdata/hadoop/mr/wc/WordCountApp.java
+++ src/main/java/com/study/bigdata/hadoop/mr/wc/WordCountApp.java
@@ -1,6 +1,7 @@
 package com.study.bigdata.hadoop.mr.wc;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -9,6 +10,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * @Author: 黄思佳
@@ -16,44 +19,50 @@ import java.io.IOException;
  */
 public class WordCountApp {
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
 
-            System.setProperty("HADOOP_USER_NAME", "hadoop");
+        String uri = "hdfs://192.168.147.3:8020";
 
-            Configuration configuration = new Configuration();
-            configuration.set("fs.defaultFS", "hdfs://192.168.147.3:8020");
+        System.setProperty("HADOOP_USER_NAME", "hadoop");
+        System.setProperty("hadoop.home.dir", "E:/hadoop-2.6.0-cdh5.15.1");
 
-            //创建job
-            Job job = Job.getInstance(configuration);
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", uri);
 
-            //设置job运行的主类
-            job.setJarByClass(WordCountApp.class);
+        //创建job
+        Job job = Job.getInstance(configuration);
 
-            //设置运行的mapper类
-            job.setMapperClass(WordCountMapper.class);
+        //设置job运行的主类
+        job.setJarByClass(WordCountApp.class);
 
-            //设置运行的reducer类
-            job.setReducerClass(WordCountReduce.class);
+        //设置运行的mapper类
+        job.setMapperClass(WordCountMapper.class);
 
-            //设置mapper(key,value)输出类型
-            job.setMapOutputKeyClass(Text.class);
-            job.setMapOutputValueClass(IntWritable.class);
+        //设置运行的reducer类
+        job.setReducerClass(WordCountReduce.class);
 
-            //设置reducer(key,value)输出类型
-            job.setOutputKeyClass(Text.class);
-            job.setOutputValueClass(IntWritable.class);
+        //设置mapper(key,value)输出类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
-            //设置文件输入输出路径
-            FileInputFormat.setInputPaths(job, new Path("/wordcount/input"));
-            FileOutputFormat.setOutputPath(job, new Path("/wordcount/output"));
+        //设置reducer(key,value)输出类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
-            boolean result = job.waitForCompletion(true);
-
-            System.exit(result ? 0 : -1);
-
-        } catch (IOException | InterruptedException | ClassNotFoundException e) {
-            e.printStackTrace();
+        //若输出目录存在文件，则递归删除
+        FileSystem fileSystem = FileSystem.get(new URI(uri), configuration, "hadoop");
+        Path ouputPath = new Path("/wordcount/output");
+        if (fileSystem.exists(ouputPath)){
+            fileSystem.delete(ouputPath, true);
         }
+
+        //设置文件输入输出路径
+        FileInputFormat.setInputPaths(job, new Path("/wordcount/input"));
+        FileOutputFormat.setOutputPath(job, ouputPath);
+
+        boolean result = job.waitForCompletion(true);
+
+        System.exit(result ? 0 : -1);
+
     }
 }
