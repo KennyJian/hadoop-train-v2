ccbc6a9489744186643619a268eb19fb4432316c
= <=>
2019-08-10 08:51:04 +0800

    词频统计

---
+++ src/main/java/com/study/bigdata/hadoop/mapreduce/project/PVStaticApp.java
@@ -0,0 +1,67 @@
+package com.study.bigdata.hadoop.mapreduce.project;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class PVStaticApp {
+    public static void main(String[] args) throws Exception{
+
+        System.setProperty("hadoop.home.dir", "D:/hadoop-2.6.0-cdh5.15.1");
+
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration);
+
+        job.setJarByClass(PVStaticApp.class);
+        job.setMapperClass(MyMapper.class);
+        job.setReducerClass(MyReduce.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(LongWritable.class);
+
+        FileSystem fileSystem = FileSystem.get(configuration);
+        Path outputPath = new Path("output/raw");
+        if (fileSystem.exists(outputPath)){
+            fileSystem.delete(outputPath, true);
+        }
+        FileInputFormat.setInputPaths(job, new Path("input/raw"));
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        job.waitForCompletion(true);
+    }
+
+    static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable>{
+
+        Text KEY = new Text("key");
+        LongWritable VALUE = new LongWritable(1);
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            context.write(KEY, VALUE);
+        }
+    }
+
+    static class MyReduce extends Reducer<Text, LongWritable, NullWritable, LongWritable>{
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            for(LongWritable value : values){
+                count++;
+            }
+            context.write(NullWritable.get(), new LongWritable(count));
+        }
+    }
+}
